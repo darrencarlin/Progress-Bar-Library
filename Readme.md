07b3c99c24e8f62abb326a8a81f8596e929c45db
Darren Carlin <darrencarlin@Darrens-MBP.home>
2018-07-15 16:05:17 -0400

    updates

--- Readme.md
+++ Readme.md
@@ -6,12 +6,18 @@
 Open the console and initialze as follows: 
 
 const scrollbar = new Scrollbar();
-
-scrollbar.add({
-     'size':'10px',
-     'color':'linear-gradient(to right, #fc466b, #3f5efb)'
-     });
+scrollbar.add();
 
 running scrollbar.add() will use default settings;
 
-currently only size and color work as custom options
+pass in an object to customise settings
+
+size, color and transition should take css styles 
+
+   settings = {
+    'pos':'top', // Position bar 'top' or 'bottom'
+    'size': '15px', 
+    'color': 'rgba(0,0,0,.6)', 
+    'transition': 'width .2s linear'
+    }
+
--- Scrollbar.js
+++ Scrollbar.js
@@ -3,6 +3,8 @@ function Scrollbar() {
 
   const [body] = [document.body];
 
+  // create element and attach attributes 
+
   const template = document.createElement('div');
   template.setAttribute('id', 'topBar');
   template.setAttribute('class', 'top-bar');
@@ -16,44 +18,62 @@ function Scrollbar() {
     size: '10px',
     width: '0px',
     top: '0px',
+    bottom: '',
     left: '0px',
-    position: 'fixed'
+    position: 'fixed',
+    transition: 'width .2s linear'
   };
 
-  // default styling
+  // default styling 
 
   Object.assign(template.style, {
     background: settings.color,
     width: settings.width,
     top: settings.top,
-    position: settings.position
+    left: settings.left,
+    position: settings.position,
+    transition: settings.transition
   });
 
   // functions
 
   const run = function run() {
+
     window.addEventListener('scroll', () => {
       const docHeight = body.scrollHeight;
       const winHeight = window.innerHeight;
       const winTop = document.scrollingElement.scrollTop;
       const scrolled = winTop / (docHeight - winHeight) * 100;
-      template.style.width = `${scrolled}%`;
+      template.style.width = `${Math.floor(scrolled)}%`;
     });
   };
 
   this.add = function add(options) {
 
+    let opts = options
+
+    // check if options are passed in as object or not at all 
+
     if (typeof options === 'object' || typeof options === 'undefined') {
 
-      // update with settings passed in
-      const newSettings = Object.assign({}, settings, options);
+      if (options.pos === 'bottom') {
+        opts.top = '';
+        opts.bottom = '0px';
+      }
+
+      // update scrollbar with new settings passed in through options
+
+      const ns = Object.assign({}, settings, options);
 
       // update style of template
       Object.assign(template.style, {
-        height: newSettings.size,
-        background: newSettings.color
-
+        height: ns.size,
+        background: ns.color,
+        transition: ns.transition,
+        top: ns.top,
+        bottom: ns.bottom
       });
+
       run();
 
     } else {
